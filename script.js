6c11019563ff520c8368c71e70fbd2add99bbcda
Abbby1007 <Abigailloken25@gmail.com>
2024-08-01 20:22:15 +0900

    Finishing the ColorWheel questions
    Finished questions 7 to 12, while also updating the home page and button style.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function cwq1()
   }
     else if(click >= 3)
      {
-      display.textContent = `Hint: ___ (3 letters)`;
+      display.textContent = `Hint: ___ (3 letters).Try Again`;
     }
   else {
     display.textContent = `Try Again`;
@@ -40,7 +40,7 @@ function cwq2()
   }
     else if(click >= 3)
      {
-      display.textContent = `Hint: RED------ (9 letters)`;
+      display.textContent = `Hint: RED------ (9 letters).Try Again`;
     }
   else {
     display.textContent = `Try Again`;
@@ -64,7 +64,7 @@ function cwq3()
   }
     else if(click >= 3)
      {
-      display.textContent = `Hint: ------ (6 letters)`;
+      display.textContent = `Hint: ------ (6 letters).Try Again`;
     }
   else {
     display.textContent = `Try Again`;
@@ -88,7 +88,7 @@ function cwq4()
   }
     else if(click >= 3)
      {
-      display.textContent = `Hint: YELLOW------ (12 letters)`;
+      display.textContent = `Hint: YELLOW------ (12 letters).Try Again`;
     }
   else {
     display.textContent = `Try Again`;
@@ -112,7 +112,175 @@ function cwq5()
   }
     else if(click >= 3)
      {
-      display.textContent = `Hint: ------ (6 letters)`;
+      display.textContent = `Hint: ------ (6 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq6()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "YELLOWGREEN"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq7.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: YELLOW----- (11 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq7()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "GREEN"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq8.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: ----- (5 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq8()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "BLUEGREEN"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq9.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: BLUE----- (9 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq9()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "BLUE"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq10.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: ---- (4 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq10()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "BLUEVIOLET"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq11.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: BLUE------ (10 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq11()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "VIOLET"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwq12.html"> <button> To Next Question </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: ------ (6 letters).Try Again`;
+    }
+  else {
+    display.textContent = `Try Again`;
+  }
+
+
+}
+
+function cwq12()
+{
+  click ++;
+  let input = document.getElementById("answerInput").value;
+  console.log(input);
+  var correct = "REDVIOLET"
+
+
+
+  if (input == correct){
+    display.textContent = `Correct`;
+    document.getElementById("appear").innerHTML = '<a href = "cwqcongratulations.html"> <button> To Congratulations Screen </button> </a>';
+  }
+    else if(click >= 3)
+     {
+      display.textContent = `Hint: RED------ (9 letters).Try Again`;
     }
   else {
     display.textContent = `Try Again`;
